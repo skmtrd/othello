34df9bab118f71e19f7b4c5aa5620410137cdfa0
skmtrd <skmtgame23@gmail.com>
2024-04-25 17:40:31 +0900

    候補表示終わった！！！！！！！！！！！！！！１

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,55 +26,41 @@ const Home = () => {
   const clickHandler = (x: number, y: number) => {
     const canPlacePoint = [];
     const newBoard = structuredClone(board);
-    function checkCanPlace(x, y) {
-      if (newBoard[y][x] === 0)
+    // for (let i = 0; i < 8; i++) {
+    //   //３になっているものを０に直す
+    //   for (let j = 0; j < 8; j++) {
+    //     if (newBoard[i][j] === 3) {
+    //       newBoard[i][j] = 0;
+    //     }
+    //   }
+    // }
+    function checkCanPlace(y, x) {
+      if (newBoard[y][x] === 0 || newBoard[y][x] === 3) {
         for (const direction of directions) {
           if (
             newBoard[y + direction[0]] !== undefined &&
             newBoard[x + direction[1]] !== undefined &&
-            newBoard[y + direction[0]][x + direction[1]] === 3 - turnColor
+            newBoard[y + direction[0]][x + direction[1]] === turnColor
           ) {
-            for (let i = 1; i < 8; i++) {
+            for (let i = 1; i < 9; i++) {
               if (
                 newBoard[y + direction[0] * i] !== undefined &&
                 newBoard[x + direction[1] * i] !== undefined
               ) {
-                if (newBoard[y + direction[0] * i][x + direction[1] * i] === 3 - turnColor) {
+                if (newBoard[y + direction[0] * i][x + direction[1] * i] === turnColor) {
                   continue;
-                } else if (newBoard[y + direction[0] * i][x + direction[1] * i] === turnColor) {
-                  console.log('pass');
+                } else if (newBoard[y + direction[0] * i][x + direction[1] * i] === 3 - turnColor) {
                   return 'True';
                 } else {
-                  return 'False';
+                  break;
                 }
               }
             }
           }
         }
+      }
     }
-    // for (let i = 0; i < 8; i++) {
-    //   for (let j = 0; j < 8; j++) {
-    //     if (newBoard[i][j] === 3) {
-    //       console.log('pass');
-    //       newBoard[i][j] = 0;
-    //     }
-    //   }
-    // }
-    // setBoard(newBoard);
-    // console.log(newBoard);
-    // for (let i = 0; i < 8; i++) {
-    //   for (let j = 0; j < 8; j++) {
-    //     if (checkCanPlace(i, j) === 'True') {
-    //       canPlacePoint[canPlacePoint.length] = [i, j];
-    //     }
-    //   }
-    // }
-    // for (const point of canPlacePoint) {
-    //   newBoard[point[0]][point[1]] = 3;
-    // }
-    // setBoard(newBoard);
     if (board[y][x] === 0 || board[y][x] === 3) {
-      const newBoard = structuredClone(board);
       for (const direction of directions) {
         const memoryPosision = [];
         if (
@@ -94,39 +80,46 @@ const Home = () => {
                 ];
                 continue;
               } else if (newBoard[y + direction[0] * i][x + direction[1] * i] === turnColor) {
-                for (let i = 0; i < 8; i++) {
-                  for (let j = 0; j < 8; j++) {
-                    if (newBoard[i][j] === 3) {
-                      console.log('pass');
-                      newBoard[i][j] = 0;
-                    }
-                  }
-                }
-                newBoard[y][x] = turnColor;
+                newBoard[y][x] = turnColor; //クリックしたところを自分の色にする
                 for (const posision of memoryPosision) {
+                  //ひっくり返し
                   newBoard[posision[0]][posision[1]] = turnColor;
                 }
-
+                console.log(newBoard);
                 for (let i = 0; i < 8; i++) {
+                  //３になっているものを０に直す
                   for (let j = 0; j < 8; j++) {
-                    if (checkCanPlace(j, i) === 'True') {
-                      console.log('pass2');
-                      canPlacePoint[canPlacePoint.length] = [i, j];
+                    if (newBoard[i][j] === 3) {
+                      newBoard[i][j] = 0;
+                      console.log('pass');
                     }
                   }
                 }
+                console.log(newBoard);
+                for (let i = 0; i < 8; i++) {
+                  //０である置くことが可能な位置を調べる、可能であればリストに格納する
+                  for (let j = 0; j < 8; j++) {
+                    console.log(i, j);
+                    if (checkCanPlace(i, j) === 'True') {
+                      canPlacePoint[canPlacePoint.length] = [i, j];
+                      console.log('passed');
+                      // newBoard[i][j] = 3;
+                    }
+                  }
+                }
+                // console.log(canPlacePoint);
                 console.log(canPlacePoint);
                 for (const point of canPlacePoint) {
+                  //格納された配置可能な座標を３にする
                   newBoard[point[0]][point[1]] = 3;
                 }
-                console.log(newBoard);
                 setTurnColor(3 - turnColor);
                 setBoard(newBoard);
                 stoneNum[0] = 0;
                 stoneNum[1] = 0;
                 for (const rows of newBoard) {
                   for (let i = 0; i < 8; i++) {
-                    if (rows[i] !== 0) {
+                    if (rows[i] !== 0 && rows[i] !== 3) {
                       rows[i] === 1 ? stoneNum[0]++ : stoneNum[1]++;
                     }
                   }
